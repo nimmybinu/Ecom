7c3d7aed3da306a8d82e826b26012b994ae09e12
nimmy <nimmimm2014@gmail.com>
2024-04-17 15:10:31 +0530

    order api

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,11 @@ const errorMiddleware = require("./middlewares/error");
 //Route imports
 const product = require("./routes/ProductRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 //middleware for errors
 app.use(errorMiddleware);
